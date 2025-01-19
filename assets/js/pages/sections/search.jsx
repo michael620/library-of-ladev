95149c03431e913f7e0d7be32edd12cd7a7fce7b
Michael <michael620@gmail.com>
2025-01-18 21:13:22 -0800

    Search title + favicon

--- assets/js/pages/sections/search.jsx
+++ assets/js/pages/sections/search.jsx
@@ -4,12 +4,14 @@ import Paper from '@mui/material/Paper';
 import { Box, LinearProgress } from '@mui/material';
 import SearchList from '@/components/SearchList.jsx';
 import SearchBar from '@/components/SearchBar.jsx';
+import { Head } from '@inertiajs/react'
 
 Search.layout = (page) => <NewAppLayout children={page} />
 export default function Search(props) {
     const [isLoading, setIsLoading] = React.useState(false);
     return (
     <Box>
+    <Head title="Library of Ladev - Search" />
     <SearchBar
         isLoading={isLoading}
         setIsLoading={setIsLoading}
